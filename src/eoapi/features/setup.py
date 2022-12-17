84fcbf6bd33a045c6ff4510e2877d502029fd45f
vincentsarago <vincent.sarago@gmail.com>
2022-12-17 12:06:05 +0100

    fix features dependencies

--- src/eoapi/features/setup.py
+++ src/eoapi/features/setup.py
@@ -10,7 +10,7 @@ inst_reqs = [
     "buildpg>=0.3",
     "fastapi>=0.73.0",
     "jinja2>=2.11.2,<4.0.0",
-    "starlette-cramjam>=0.1.0,<0.2",
+    "starlette-cramjam>=0.3,<0.4",
     "geojson_pydantic",
     "orjson",
 ]
