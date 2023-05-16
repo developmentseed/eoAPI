78f6b9cf0b4a041ad75043b7ffffe9da632e3f64
ranchodeluxe <greg.corradini@gmail.com>
2023-05-16 05:33:19 -0700

    combine into 'manifest.yaml'

--- infrastructure/k8s/helm-chart/eoapi/helm-template.sh
+++ infrastructure/k8s/helm-chart/eoapi/helm-template.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 MANIFEST=/tmp/eoapi-manfests.yaml
 echo "" > $MANIFEST
-helm template . -s templates/vector/deployment.yaml --set gitSha=$(git rev-parse HEAD | cut -c1-10) -f values.yaml >> $MANIFEST
-helm template . -s templates/vector/configmap.yaml --set gitSha=$(git rev-parse HEAD | cut -c1-10) -f values.yaml >> $MANIFEST
-helm template . -s templates/vector/service.yaml --set gitSha=$(git rev-parse HEAD | cut -c1-10) -f values.yaml >> $MANIFEST
-helm template . -s templates/db/postgres.yaml --set gitSha=$(git rev-parse HEAD | cut -c1-10) -f values.yaml >> $MANIFEST
+MANIFESTS=$(find ./templates/* -name "manifest.yaml")
+while read MFILE; do
+  path_without_dot_prefix=$(echo "$MFILE" | sed 's/^\.\///g')
+  printf "[ RENDERING ]: %s\n" "$path_without_dot_prefix"
+  helm template . -s $path_without_dot_prefix --set gitSha=$(git rev-parse HEAD | cut -c1-10) -f values.yaml >> $MANIFEST
+done < <(echo "$MANIFESTS")
+
