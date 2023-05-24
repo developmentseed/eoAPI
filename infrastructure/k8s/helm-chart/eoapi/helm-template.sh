1feb7d3831850596be267e2d926eecc5e52e2404
ranchodeluxe <greg.corradini@gmail.com>
2023-05-23 17:55:36 -0700

    unit test scaffolding

--- infrastructure/k8s/helm-chart/eoapi/helm-template.sh
+++ infrastructure/k8s/helm-chart/eoapi/helm-template.sh
@@ -8,7 +8,7 @@ export GITSHA=$(git rev-parse HEAD | cut -c1-10)
 MANIFEST=./tmp/eoapi-manfests.yaml
 
 echo "" > $MANIFEST
-MANIFESTS=$(find ./templates/* -name "manifest.yaml")
+MANIFESTS=$(find ./templates/* -name "*.yaml")
 while read MFILE; do
   path_without_dot_prefix=$(echo "$MFILE" | sed 's/^\.\///g')
   printf "[ RENDERING ]: %s\n" "$path_without_dot_prefix"
