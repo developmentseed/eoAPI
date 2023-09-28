01a2bb2c4f1166522413f4c8b4588c99b52e7375
Aimee Barciauskas <aimee@developmentseed.org>
2023-09-28 12:18:25 -0700

    Add CMIP6 demo

---
+++ demo/cmip6/seed-db.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+model="$1"
+variable="$2"
+
+collection_json_file="CMIP6_daily_${model}_${variable}_collection.json"
+items_json_file="CMIP6_daily_${model}_${variable}_stac_items.ndjson"
+
+if [ -z "$DATABASE_URL" ]; then
+    username=postgres
+    password=password
+    host=localhost
+    dbname=postgres
+    port=5432
+    DATABASE_URL="postgresql://$username:$password@$host:$port/$dbname"
+fi
+
+echo "Inserting collection from $collection_json_file"
+pypgstac load collections $collection_json_file --dsn $DATABASE_URL --method insert_ignore
+echo "Inserting items from $items_json_file"
+pypgstac load items $items_json_file --dsn $DATABASE_URL --method insert_ignore
